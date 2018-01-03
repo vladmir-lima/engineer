89f7b6375df90ec7062d8424d4207e3fd3c0850b
vladmir-lima <vladmir@gmail.com>
2018-01-03 00:30:52 -0200

    Commit inicial do projeto

---
+++ src/app/pages/pages.component.ts
@@ -0,0 +1,20 @@
+﻿import { Component } from '@angular/core';
+import { Routes } from '@angular/router';
+
+import { BaMenuService } from '../theme';
+import { PAGES_MENU } from './pages.menu';
+
+@Component({
+  selector: 'pages',  
+  styleUrls: ['./page.component.scss'],
+  templateUrl: './page.component.html'
+})
+export class Pages {
+
+  constructor(private _menuService: BaMenuService,) {
+  }
+
+  ngOnInit() {
+    this._menuService.updateMenuByRoutes(<Routes>PAGES_MENU);
+  }
+}
