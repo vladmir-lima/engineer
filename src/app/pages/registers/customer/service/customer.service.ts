81289424d3301659b7053c670fb5c07b29d72d5f
Vladmir Lima <vladmirl@gmail.com>
2018-01-16 19:41:18 -0200

    edição do cadastro de cliente

--- src/app/pages/registers/customer/service/customer.service.ts
+++ src/app/pages/registers/customer/service/customer.service.ts
@@ -33,7 +33,7 @@ export class CustomerService {
     },
     {
       'id': 5,
-      'nomeFantasia': 'Wing',
+      'nomeFantasia': 'Vlad',
       'inscricaoEstadual': '1234567',
       'razaoSocial': 'Vladmir',
       'email': 'mi.Aliquam@Phasellus.net',
